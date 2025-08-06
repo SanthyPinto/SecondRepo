421ac7a2473d932f3bf5680fac98a733fd79e48c
Santiago <israsta2020@gmail.com>
2025-08-05 20:06:35 -0500

    fix: remove secret key

---
+++ project1/testing/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Platzi!");
+
+string testingConnection = "Server=localhost;Database=mi_base;User Id=root;Password=1234;";
